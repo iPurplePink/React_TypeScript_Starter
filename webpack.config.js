85fff6fc0d7ae7ee1b3486fb40f87ccc5a61e803
Maridin San Miguel <maridinsanmiguel@gmail.com>
2020-05-02 20:24:49 +0800

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  alias: {
+    '@src': path.join(__dirname, '../src'),
+    '@components': path.join(__dirname, '../src/components'),
+    '@css': path.join(__dirname, '../src/css'),
+    '@modules': path.join(__dirname, '../src/modules'),
+  },
+};
